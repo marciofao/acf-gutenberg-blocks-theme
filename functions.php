9b65fc5ec9f353ea6c83e759a0056302adb73ad1
marciofao <marcio.lopes.fao@gmail.com>
2023-04-22 19:20:04 -0300

    separating dinamic content from static

--- functions.php
+++ functions.php
@@ -1,14 +1,19 @@
 <?php
 //die(get_stylesheet_directory() . 'blocks/block-portfolio-item.php');
 
-function myfirsttheme_setup() {
+function agb_theme_setup() {
     /*
      * Load bootstrap and other css assets
      */
     wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', false, '1.1', 'all');
+    wp_enqueue_style('font_awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css', false, '1.1', 'all');
+
+    wp_enqueue_style('default_css', get_template_directory_uri() . '/css/style.css', false, '1.1', 'all');
+
+
 
 }
-add_action('after_setup_theme', 'myfirsttheme_setup');
+add_action('after_setup_theme', 'agb_theme_setup');
 
 // check function exists
 if (function_exists('acf_register_block_type')) {
@@ -32,3 +37,18 @@ function register_acf_block_types() {
         )
     );
 }
+
+
+add_theme_support('custom-logo');
+function agb_custom_logo_setup() {
+    $defaults = array(
+        'height'               => 100,
+        'width'                => 400,
+        'flex-height'          => true,
+        'flex-width'           => true,
+        'header-text'          => array('site-title', 'site-description'),
+        'unlink-homepage-logo' => true,
+    );
+    add_theme_support('custom-logo', $defaults);
+}
+add_action('after_setup_theme', 'agb_custom_logo_setup');
